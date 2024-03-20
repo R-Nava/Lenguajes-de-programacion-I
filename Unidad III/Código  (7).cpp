63499f46a3675f4f3a787a39182ab653bc5acec1
R-Nava <141874795+R-Nava@users.noreply.github.com>
2024-03-19 18:06:10 -0600

    Update Código  (7).cpp

--- Unidad III/Código  (7).cpp
+++ Unidad III/Código  (7).cpp
@@ -2,59 +2,49 @@
 #include <math.h>
 #include <string.h>
 
-int main (){
-	
-	float test, num;
-	int palabras, tam, x, y, conta=0;
-	char matriz [10][20];
-	
-	do{ //validacion de entero
-		printf ("Ingresa cuantas palabras: \n");
-		scanf ("%f", &num);
-		test = trunc (num);
-		if ((num-test) != 0){
-			printf ("Este numero debe ser entero \n");
-		}else{
-			palabras=num;	
-		}
-		if (num>10){
-			printf ("No puede ingresar m�s de 10 palabras \n");
-		}
-	}while((num-test) != 0 || num>10); //validacion de entero
-	
-	for (y=0;y<palabras;y++){
-		
-		printf ("Dame el escrito %d: \n", y);
-		fflush (stdin);
-		gets(matriz[y]);
-		
-	}
-	
-	for (y=0;y<palabras;y++){
-	
-		tam=strlen(matriz[y]);
-		
-		for (x=0;x<tam;x++){
-			
-			if ((matriz[y][x]>='a' && matriz[y][x]<='z') || (matriz[y][x]>='A' && matriz[y][x]<='Z')){
-				
-				if (matriz[y][x]!='a' && matriz[y][x]!='A'
-				 && matriz[y][x]!='e' && matriz[y][x]!='E'
-				 && matriz[y][x]!='i' && matriz[y][x]!='I'
-				 && matriz[y][x]!='o' && matriz[y][x]!='O'
-				 && matriz[y][x]!='u' && matriz[y][x]!='U'){
-				 	
-				 	conta++;
-				 	
-				 }
-				
-			}
-			
-		}
-	
-	}
-	
-	printf ("La cantidad de consonantes es: %d \n", conta);
-	
-	return 0;
+int main() {
+    int palabras, tam, x, y, conta = 0;
+    char matriz[10][20];
+    float num;
+
+    do {
+        printf("Ingresa cuantas palabras: \n");
+        scanf("%f", &num);
+
+        if (floor(num) != num || num > 10 || num <= 0) {
+            printf("Este numero debe ser un entero entre 1 y 10 \n");
+        } else {
+            palabras = num;
+        }
+    } while (floor(num) != num || num > 10 || num <= 0);
+
+    getchar(); // Limpiar el buffer de entrada
+
+    for (y = 0; y < palabras; y++) {
+        printf("Dame el escrito %d: \n", y);
+        fgets(matriz[y], sizeof(matriz[y]), stdin);
+
+        // Eliminar el carácter de nueva línea del final de la cadena
+        if (matriz[y][strlen(matriz[y]) - 1] == '\n') {
+            matriz[y][strlen(matriz[y]) - 1] = '\0';
+        }
+    }
+
+    for (y = 0; y < palabras; y++) {
+        tam = strlen(matriz[y]);
+
+        for (x = 0; x < tam; x++) {
+            if ((matriz[y][x] >= 'a' && matriz[y][x] <= 'z') || (matriz[y][x] >= 'A' && matriz[y][x] <= 'Z')) {
+                if (matriz[y][x] != 'a' && matriz[y][x] != 'A' && matriz[y][x] != 'e' && matriz[y][x] != 'E' &&
+                    matriz[y][x] != 'i' && matriz[y][x] != 'I' && matriz[y][x] != 'o' && matriz[y][x] != 'O' &&
+                    matriz[y][x] != 'u' && matriz[y][x] != 'U') {
+                    conta++;
+                }
+            }
+        }
+    }
+
+    printf("La cantidad de consonantes es: %d \n", conta);
+
+    return 0;
 }
