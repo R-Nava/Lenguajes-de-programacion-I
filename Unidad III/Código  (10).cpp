1226a76b7de0d1ea91db8cd65a3e4e8947f50e9c
R-Nava <141874795+R-Nava@users.noreply.github.com>
2024-03-19 18:16:36 -0600

    Update Código  (10).cpp

--- Unidad III/Código  (10).cpp
+++ Unidad III/Código  (10).cpp
@@ -1,18 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int main (){	
-	char escrito[30];
-	int tam,i,cont=0;
-	printf ("Ingrese el escrito: \n");
-	fflush (stdin);
-	gets (escrito);
-	tam = strlen (escrito); 
-	for (i=0; i<tam; i++){
-		if (escrito[i]!='a' and escrito[i]!='e' and escrito[i]!='i' and escrito[i]!='o' and escrito[i]!='u'){
-		cont++;
-		}
-	}
-	printf ("%s Tiene %d consonantes: \n",escrito,cont);
-	return 0;
+
+int main() {
+    char escrito[30];
+    int tam, i, cont = 0;
+
+    printf("Ingrese el escrito: \n");
+    fflush(stdin);
+    gets(escrito);
+
+    tam = strlen(escrito);
+
+    for (i = 0; i < tam; i++) {
+        if (escrito[i] != 'a' && escrito[i] != 'e' && escrito[i] != 'i' && escrito[i] != 'o' && escrito[i] != 'u' &&
+            escrito[i] != 'A' && escrito[i] != 'E' && escrito[i] != 'I' && escrito[i] != 'O' && escrito[i] != 'U') {
+            cont++;
+        }
+    }
+
+    printf("%s Tiene %d consonantes: \n", escrito, cont);
+
+    return 0;
 }
